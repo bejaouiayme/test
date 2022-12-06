da77db6ba7be308406bae023c6d22608c2e95d7e
bejaouiayme <aymenbjeoui76@gmail.com>
2022-12-06 16:36:09 +0100

    e1

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int nbr;
+
+   printf("Entrez un entier:\n");
+   scanf("%d", &nbr);
+
+   if (nbr % 2 == 0)
+      printf("Paire\n");
+   else
+      printf("Impaire\n");
+
+   return 0;
+}
---
+++ test2.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    int somme = 5 + 3;
+
+    printf("5 + 3 = %d\n", somme);
+    return 0;
+}
